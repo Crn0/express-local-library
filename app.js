b92307921935097a7455b77f53bf8a9fe7153cf5
Crno0 <123456789+odin@users.noreply.github.com>
2024-04-30 13:13:26 +0800

    Add clean app

---
+++ app.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import createError from 'http-errors';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
+import catalogRouter from './routes/catalog.js';
+
+const app = express();
+
+// Set up mongoose connection
+// Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
+// Included because it removes preparatory warnings for Mongoose 7.
+// See: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
+mongoose.set('strictQuery', false);
+// Define the database URL to connect to.
+const mongoDb = 'mongodb url';
+// Wait for database to connect, logging an error if there is a problem
+const main = async () => await mongoose.connect(mongoDb);
+main().catch(console.error);
+
+// Init dir name variable
+const __dirname = import.meta.dirname;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+export default app;
