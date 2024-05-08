0826329be2f3e10d3fefdede199ae93e2af4e7de
Crno0 <123456789+odin@users.noreply.github.com>
2024-05-08 16:57:03 +0800

    Change max request

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 // Set up rate limiter: maximum of twenty requests per minute
 const limiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 1 minute
-    max: 2000,
+    max: 20,
 });
 // Apply rate limiter to all requests
 app.use(limiter);
